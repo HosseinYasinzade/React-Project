67d92e6d112f41e454fd9489e4ddfd7b144f5f4b
Hossein <hosseinyasinzadeh2002@gmail.com>
2025-01-06 12:07:15 +0330

    first commit

---
+++ TodoList/src/Components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center bg-green-600 h-32 px-24">
+      <h1 className="text-white font-bold text-4xl">Todo App</h1>
+      <div className="form-container">
+        <form>
+          <input
+            type="text"
+            placeholder="Todo Title"
+            className="border-none outline-none h-10 w-80 rounded-3xl px-4 mr-3"
+          />
+          <input
+            type="submit"
+            value="Submit"
+            className="bg-green-800 text-white w-24 h-12 rounded-3xl"
+          />
+        </form>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
