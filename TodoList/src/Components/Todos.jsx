9689ee3969cad0f11d2da53814aceea59fd66c05
Hossein <hosseinyasinzadeh2002@gmail.com>
2025-01-06 21:54:16 +0330

    feat: add complete todo

--- TodoList/src/Components/Todos.jsx
+++ TodoList/src/Components/Todos.jsx
@@ -20,6 +20,14 @@ const Todos = () => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  const handleCompleted = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
   return (
     <section className="flex flex-col justify-center items-center text-white">
       <h1 className="text-3xl font-bold mb-6">Todos List</h1>
@@ -27,7 +35,10 @@ const Todos = () => {
         {todos.map((todo) => (
           <li
             key={todo.id}
-            className="flex justify-between items-center text-2xl px-8 bg-green-600 w-1/2 h-16 rounded-2xl my-4 shadow-lg"
+            className={`flex justify-between items-center text-2xl px-8 cursor-pointer bg-green-600 w-1/2 h-16 rounded-2xl my-4 shadow-lg ${
+              todo.completed ? "line-through" : ""
+            }`}
+            onClick={() => handleCompleted(todo.id)}
           >
             {todo.title}
             <button onClick={() => handleDelete(todo.id)}>
