658fcd281ca3bd35c8480d3a76739dc1123cf8d0
Hossein <hosseinyasinzadeh2002@gmail.com>
2025-01-07 22:58:50 +0330

    fin: finish counter

--- counter/src/components/Counter.jsx
+++ counter/src/components/Counter.jsx
@@ -1,17 +1,41 @@
+import { useState } from "react";
+
 const Counter = () => {
+  const handleDecrement = () =>
+    setCount((count) => {
+      if (count === 0) {
+        return 0;
+      }
+      return count - 1;
+    });
+
+  const handleIncrement = () => setCount((count) => count + 1);
+
+  const handleReset = () => setCount(0);
+
+  const [count, setCount] = useState(0);
   return (
     <main className="flex flex-col justify-center items-center text-white font-bold text-5xl">
       <h1 className="mt-40">Counter</h1>
       <div className="flex justify-center items-center gap-4 mt-8">
-        <button className="px-3 py-2 shadow-md shadow-pink-800 rounded-md">
+        <button
+          className="px-3 py-2 shadow-md shadow-pink-800 rounded-md"
+          onClick={handleDecrement}
+        >
           -
         </button>
-        <p>0</p>
-        <button className="px-2 py-2 shadow-md shadow-pink-800 rounded-md">
+        <p>{count}</p>
+        <button
+          className="px-2 py-2 shadow-md shadow-pink-800 rounded-md"
+          onClick={handleIncrement}
+        >
           +
         </button>
       </div>
-      <button className="w-40 h-10 rounded-md mt-8 bg-pink-800 text-3xl">
+      <button
+        className="w-40 h-10 rounded-md mt-8 bg-pink-800 text-3xl"
+        onClick={handleReset}
+      >
         Reset
       </button>
     </main>
