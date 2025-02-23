2234cde032bab4610bf3e9af6ca2626209756e16
Hossein Yasinzadeh <hosseinyasinzadeh2002@gmail.com>
2025-02-23 12:13:10 +0330

    feat: create simple ui

--- weather-app/context/WeatherContext.jsx
+++ weather-app/context/WeatherContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useState } from "react";
 import axios from "axios";
 
 export const WeatherContext = createContext();
--- weather-app/src/Components/Weather.jsx
+++ weather-app/src/Components/Weather.jsx
@@ -5,16 +5,31 @@ const WeatherDisplay = () => {
   const { search, error, result, handleChange, handleSubmit } = useWeather();
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={search} onChange={handleChange} />
-        <button type="submit">Search</button>
+    <div className="container">
+      <form onSubmit={handleSubmit} className="form">
+        <input
+          type="text"
+          value={search}
+          onChange={handleChange}
+          className="search_input"
+        />
+        <button type="submit" className="search_btn">
+          Search
+        </button>
       </form>
 
       <div>
         {error && <p>{error.message}</p>}
-        {result ? <h1>{result.location.name}</h1> : <h1>City Name</h1>}
-        {result ? <h2>{result.current.temp_c}°C</h2> : <h2>Temp</h2>}
+        {result ? (
+          <h1 className="location">{result.location.name}</h1>
+        ) : (
+          <h1 className="location">City Name</h1>
+        )}
+        {result ? (
+          <h2 className="temp">{result.current.temp_c}°C</h2>
+        ) : (
+          <h2 className="temp">Temp</h2>
+        )}
       </div>
     </div>
   );
